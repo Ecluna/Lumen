1a5345075a6572f306c1fc899fdcb5f55509407f
Ecluna <168729200@qq.com>
2024-12-29 22:26:02 +0800

    ♻️ refactor(toolbar): reorganize button order

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,23 @@
     <div class="toolbar">
       <!-- 左侧按钮组 -->
       <div class="toolbar-left">
-        <button class="toolbar-btn" @click="toggleFileManager" title="文件管理器">
-          <svg viewBox="0 0 16 16" width="16" height="16">
-            <path fill="currentColor" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3H7.5a.25.25 0 01-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75z"/>
-          </svg>
-        </button>
+        <!-- 新建文件按钮 -->
         <button class="toolbar-btn" @click="createNewFile" title="新建文件">
           <svg viewBox="0 0 16 16" width="16" height="16">
             <path fill="currentColor" d="M3.75 1.5a.25.25 0 0 0-.25.25v11.5c0 .138.112.25.25.25h8.5a.25.25 0 0 0 .25-.25V6H9.75A1.75 1.75 0 0 1 8 4.25V1.5H3.75zm5.75.56v2.19c0 .138.112.25.25.25h2.19L9.5 2.06zM2 1.75C2 .784 2.784 0 3.75 0h5.086c.464 0 .909.184 1.237.513l3.414 3.414c.329.328.513.773.513 1.237v8.086A1.75 1.75 0 0 1 12.25 15h-8.5A1.75 1.75 0 0 1 2 13.25V1.75z"/>
             <path fill="currentColor" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        </button>
+        <!-- 打开文件按钮 -->
+        <button class="toolbar-btn" @click="openFile" title="打开文件">
+          <svg viewBox="0 0 16 16" width="16" height="16">
+            <path fill="currentColor" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+          </svg>
+        </button>
+        <!-- 最近打开按钮 -->
+        <button class="toolbar-btn" @click="toggleFileManager" title="最近打开">
+          <svg viewBox="0 0 16 16" width="16" height="16">
+            <path fill="currentColor" d="M1.75 1A1.75 1.75 0 000 2.75v10.5C0 14.216.784 15 1.75 15h12.5A1.75 1.75 0 0016 13.25v-8.5A1.75 1.75 0 0014.25 3H7.5a.25.25 0 01-.2-.1l-.9-1.2C6.07 1.26 5.55 1 5 1H1.75z"/>
           </svg>
         </button>
       </div>
@@ -34,11 +42,6 @@
 
       <!-- 右侧按钮组 -->
       <div class="toolbar-right">
-        <button class="toolbar-btn" @click="openFile" title="打开文件">
-          <svg viewBox="0 0 16 16" width="16" height="16">
-            <path fill="currentColor" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-          </svg>
-        </button>
         <button class="toolbar-btn" @click="saveFile" title="保存文件">
           <svg viewBox="0 0 16 16" width="16" height="16">
             <path fill="currentColor" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"/>
