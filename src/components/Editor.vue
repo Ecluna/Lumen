061d94963a81adfa3871da153b45ffbf83361f40
Ecluna <168729200@qq.com>
2024-12-29 20:20:08 +0800

    ✨ feat(editor): enhance outline toggle button

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -42,9 +42,14 @@
             :class="{ 'active': showOutline }"
             @click="toggleOutline" 
             title="切换大纲">
-            <svg viewBox="0 0 16 16" width="16" height="16">
-              <path fill="currentColor" d="M2 2.5A.5.5 0 0 1 2.5 2h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
-            </svg>
+            <div class="icon-wrapper">
+              <svg class="icon-menu" viewBox="0 0 16 16" width="16" height="16">
+                <path fill="currentColor" d="M2 2.5A.5.5 0 0 1 2.5 2h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+              <svg class="icon-close" viewBox="0 0 16 16" width="16" height="16">
+                <path fill="currentColor" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </div>
           </button>
         </div>
         <div class="status-right">
@@ -585,5 +590,34 @@ defineExpose({
 .status-btn.active {
   background: #e1e4e8;
   color: #24292e;
+}
+
+.icon-wrapper {
+  position: relative;
+  width: 16px;
+  height: 16px;
+}
+
+.icon-menu,
+.icon-close {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: opacity 0.3s ease, transform 0.3s ease;
+}
+
+.icon-close {
+  opacity: 0;
+  transform: rotate(-180deg);
+}
+
+.status-btn.active .icon-menu {
+  opacity: 0;
+  transform: rotate(180deg);
+}
+
+.status-btn.active .icon-close {
+  opacity: 1;
+  transform: rotate(0);
 }
 </style> 
